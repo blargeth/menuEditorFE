bed4672ce927e517c77e74ed7d9dfbe9a8f8a5b7
blargeth <albertkchua@gmail.com>
2021-02-09 09:40:23 -0500

    using webpack instead of create-react-app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import './App.css';
-import Button from './components/button';
-import axios from 'axios';
 
-let APIgetRequest = () => {
-  console.log('running the api request!!')
+import React from "react";
 
-  return axios.get(`https://trackapi.nutritionix.com/v2/search/instant?query=potato`, {
-    headers: {
-        'x-app-id': 'a68bfb58',
-        'x-app-key': '873ad07273ee8534c33943bf98f26e23',
-      }
-    })
-    .then(result => {
-      console.log(result)
-      return result})
-};
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello! I'm the test app!
-          <Button clickFunc={APIgetRequest}/>
-        </p>
-
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1>
+          Hello {name}
+        </h1>
+      </>
+    );
+  }
 }
 
 export default App;
